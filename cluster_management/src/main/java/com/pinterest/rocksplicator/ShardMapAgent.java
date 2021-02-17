c125c4fd830b9390fd37f088376f1044b5c273d6
Gopal Rajpurohit <grajpurohit@pinterest.com>
2021-02-16 20:23:02 -0800

    Initial ShardMapAgent

---
+++ cluster_management/src/main/java/com/pinterest/rocksplicator/ShardMapAgent.java
@@ -0,0 +1,58 @@
+package com.pinterest.rocksplicator;
+
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ShardMapAgent {
+  private static final Logger LOG = LoggerFactory.getLogger(ShardMapAgent.class);
+
+  private static final String zkShardMapSvrArg = "zkShardMapSvr";
+  private static final String clusterArg = "cluster";
+  private static final String clustersFileArg  = "clustersFile";
+  private static final String shardMapDirArg = "shardMapDir";
+
+  private static Options constructCommandLineOptions() {
+    Option zkShardMapSvrOption = OptionBuilder
+        .withLongOpt(zkShardMapSvrArg)
+        .withDescription("Provide zk server connect string hosting the shard_maps [Required]").create();
+    zkShardMapSvrOption.setArgs(1);
+    zkShardMapSvrOption.setRequired(true);
+    zkShardMapSvrOption.setArgName(zkShardMapSvrArg);
+
+    Option clusterOption = OptionBuilder
+        .withLongOpt(clusterArg)
+        .withDescription("Provide cluster to download shard_map for [Optional]").create();
+    zkShardMapSvrOption.setArgs(1);
+    zkShardMapSvrOption.setRequired(false);
+    zkShardMapSvrOption.setArgName(clusterArg);
+
+    Option clustersFileOption = OptionBuilder
+        .withLongOpt(clustersFileArg)
+        .withDescription("Provide file path containing clusters to download shard_maps [Optional,"
+            + " at least one of cluster or clustersFile argument must be provided ]").create();
+    zkShardMapSvrOption.setArgs(1);
+    zkShardMapSvrOption.setRequired(false);
+    zkShardMapSvrOption.setArgName(clustersFileArg);
+
+    Option shardMapDirOption = OptionBuilder
+        .withLongOpt(shardMapDirArg)
+        .withDescription("Provide directory to download shardMap for each cluster").create();
+    zkShardMapSvrOption.setArgs(1);
+    zkShardMapSvrOption.setRequired(true);
+    zkShardMapSvrOption.setArgName(shardMapDirArg);
+
+    Options options = new Options();
+    options.addOption(zkShardMapSvrOption)
+        .addOption(clusterOption)
+        .addOption(clustersFileOption);
+
+    return options;
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
